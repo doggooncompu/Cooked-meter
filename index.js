e9df8344889770758074faaa40b98ea439a4b41c
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-10-21 18:01:58 +0000

    yes

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 const express = require("express")
+const { readFile } = require("fs")
+const { getSystemErrorMap } = require("util")
 const app = express()
 const port = 8080
+
+const fs = require("fs").promises
+
+async function readFile(){
+  fs.readFile("cooked.txt","utf-8", (err, data) =>{
+    if(err){
+      console.log(err)
+    }
+
+    var json_data = data.parse()
+    return json_data
+  })
+}
 
 app.get("/", (req,res) =>{
   res.sendFile(__dirname+"/index.html")
 });
+
+
 
 
 app.listen(port, () => {
