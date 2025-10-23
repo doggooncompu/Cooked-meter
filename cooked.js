6a43001d86ec0bf7d13c26b05dff64408c7a52f9
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-10-23 17:59:54 +0000

    j\

--- cooked.js
+++ cooked.js
@@ -1,7 +1,7 @@
 async function loadmeter(){
     const url = "doggooncompu.github.io/CS-club-intro/meter";
     var data = await fetch(url, {
-        "method":"POST"
+        "method":"GET"
     });
     console.log(data);
 }
