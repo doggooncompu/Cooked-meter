059387382374a7a49b9330e88243ffc2fa5b096d
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-10-27 15:43:54 +0000

    h

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req,res) =>{
   res.sendFile(__dirname+"/index.html")
 });
 
-app.get("/Cooked-meter/meter", (req,res)=>{
+app.get("/meter", (req,res)=>{
   res.sendFile(__dirname+"/cooked.json");
 })
 app.get("/Cooked-meter/test", (req,res)=>{
