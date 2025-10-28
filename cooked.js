80f4362b3ac0bab35d76835afcc393776ca9b90e
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-10-28 03:10:51 +0000

    quoi

--- cooked.js
+++ cooked.js
@@ -1,4 +1,4 @@
-async function loadmeter(){
+function loadmeter(){
     const url = "https://cooked-meter-phi.vercel.app/meter";
     fetch(url)
   .then(response => response.json()) // Parses the response body as JSON
@@ -6,13 +6,13 @@ async function loadmeter(){
     var div = document.getElementById("cooked");
 
     var label = document.createElement("p");
-    label.textContent = "subject:" + data.subject + "percentage: ", data.cooked_level;
+    label.textContent = "subject:" + data.subject + " percentage: ", data.cooked_level;
 
     var meter = document.createElement("meter");
     meter.value = data.cooked_level;
 
     console.log(data.cooked_level);
-    
+
     div.append(meter);
     div.append(label);
   })
