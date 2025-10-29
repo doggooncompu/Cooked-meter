f34695ee5d6ebe6c93e3729e1592e4fc9e9f772c
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-10-29 15:59:14 +0000

    login pt1

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 
 const express = require("express")
+const fs = require("fs");
+const file = require(__dirname+"/cooked.json");
+
 const app = express()
 const port = 8080
+const session = require('express-session');
+app.use(session({
+  secret: 'your_secret_key', // A secret key to sign the session ID cookie. Replace with a strong, unique value.
+    resave: false, // Don't save session if unmodified
+      saveUninitialized: true, // Save uninitialized sessions
+      cookie: { secure: true } // Set to true if using HTTPS
+    }));
 
 
 app.get("/", (req,res) =>{
   res.sendFile(__dirname+"/index.html")
 });
 
+app.get("/login", (req,res)=>{
+  res.sendFile(__dirname+"/login.html");
+})
+
 app.get("/meter", (req,res)=>{
   res.contentType("json");
   res.sendFile(__dirname+"/cooked.json");
 })
-app.get("/Cooked-meter/test", (req,res)=>{
-  res.send("<h1>hello</h1>");
+
+
+app.post("/logsub", (req,res)=>{
+
+  if(req.body.password == "password"){
+    req.session.password = "password";
+    console.log("Usr authenticated!");
+    res.send("authenticated");
+  }
+  else{
+    res.send("wrong password");
+  }
+
 })
+
+app.post("/submit", (req,res)=>{
+  if(req.session.password == "password"){
+    const subject = req.body.subject;
+    const level = req.body.level;
+    var content = {"subject": subject, "cooked_level": level};
+
+    console.log("authenticated user submitted a change");
+
+    fs.writeFile(__dirname+"/cooked.json",content, (err) =>{
+
+      if(err){
+        console.log(err);
+      }
+      console.log("sucessfully written!");
+
+    })
+    res.send("change submitted");
+  }
+  else{
+    res.send("not authenticated!");
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
