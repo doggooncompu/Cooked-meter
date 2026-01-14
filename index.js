7f790295e7ac2fda1de904c3d377f51eb487ef67
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2026-01-14 18:06:56 +0000

    ggg

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(session({
     }));
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
